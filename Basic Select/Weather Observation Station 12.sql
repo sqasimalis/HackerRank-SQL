1115a6c104a61d9b260df112f9f94b80b93a9122
Syed Qasim Ali Shah <syedqasimali311@gmail.com>
2025-01-11 18:07:41 +0100

    Add files via upload

---
+++ Basic Select/Weather Observation Station 12.sql
@@ -0,0 +1,8 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels
+and do not end with vowels. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT city
+FROM station
+WHERE LEFT(city, 1) NOT IN ('A','E','I','O','U')
+  AND RIGHT(city, 1) NOT IN ('a','e','i','o','u');
