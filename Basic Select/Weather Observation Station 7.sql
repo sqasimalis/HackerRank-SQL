418e9013b719c2289620a9f9feac1537afb9642b
Syed Qasim Ali Shah <syedqasimali311@gmail.com>
2025-01-11 18:05:00 +0100

    Add files via upload

---
+++ Basic Select/Weather Observation Station 7.sql
@@ -0,0 +1,7 @@
+/*
+Query the list of CITY names ending with vowels (a, e, i, o, u)from STATION.
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT city
+FROM station
+WHERE RIGHT(city, 1) IN ('a','e','i','o','u');
