f9827d4c68391c33ae68f5c092c45d38a3fe3ded
Syed Qasim Ali Shah <syedqasimali311@gmail.com>
2025-01-11 18:05:19 +0100

    Add files via upload

---
+++ Basic Select/Weather Observation Station 8.sql
@@ -0,0 +1,9 @@
+/*
+Query the list of CITY names from STATION which have vowels
+(i.e., a, e, i, o, and u) as both their first and last characters.
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT city
+FROM station
+WHERE LEFT(city, 1) IN ('A','E','I','O','U')
+  AND RIGHT(city, 1) IN ('a','e','i','o','u');
