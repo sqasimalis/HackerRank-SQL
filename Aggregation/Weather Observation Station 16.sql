94de0cfd1edf056979ec0344a670fdd8007a716a
Syed Qasim Ali Shah <syedqasimali311@gmail.com>
2025-01-15 21:34:30 +0100

    Add files via upload

---
+++ Aggregation/Weather Observation Station 16.sql
@@ -0,0 +1,10 @@
+/*
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than
+38.7780. Round your answer to 4 decimal places.
+*/
+SELECT
+    ROUND(MIN(lat_n), 4)
+FROM
+    station
+WHERE
+    lat_n > 38.7780;
